c8436ca1bcc06ea60ce2700b4aa1408bec43ccc3
JordanRBurke <jordanrobertburke@gmail.com>
2018-04-27 13:02:34 -0400

    Initial commit

---
+++ app/src/main/java/com/jordanburke/wowmountandachieventinfo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jordanburke.wowmountandachieventinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
